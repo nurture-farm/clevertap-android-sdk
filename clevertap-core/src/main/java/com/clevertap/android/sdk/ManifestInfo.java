18d7abde6204a830e50acc9e7ee9b2ff08e57710
debojyoti <debojyoti.karmakar@neebal.com>
2024-02-26 17:39:52 +0530

    Refactor

--- clevertap-core/src/main/java/com/clevertap/android/sdk/ManifestInfo.java
+++ clevertap-core/src/main/java/com/clevertap/android/sdk/ManifestInfo.java
@@ -197,10 +197,6 @@ public class ManifestInfo {
         return accountRegion;
     }
 
-    String getProxyDomain() {
-        return proxyDomain;
-    }
-
     String getAcountToken() {
         return accountToken;
     }
@@ -242,9 +238,6 @@ public class ManifestInfo {
         return useADID;
     }
 
-    String getUserType() { return userType; }
-
-    static void changeCredentials(String id, String token, String region, String proxy) {
     boolean isUseCustomDeviceId() {
         return useCustomDeviceId;
     }
@@ -260,7 +253,6 @@ public class ManifestInfo {
         accountId = id;
         accountToken = token;
         accountRegion = region;
-        proxyDomain = proxy;
     }
 
     static void changeCredentials(String id, String token, String _proxyDomain, String _spikyProxyDomain) {
